c9b38a6e48b46cbdffbe87355f9df9715f3c8162
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2024-10-12 20:45:33 -0300

    Add import Angular Material components

---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ]
+})
+export class MaterialModule { }
