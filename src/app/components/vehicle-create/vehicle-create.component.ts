f925ad5c511173e273ef6aa37905cee9317eb11f
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2024-10-13 15:33:37 -0300

    Add create vehicle

--- src/app/components/vehicle-create/vehicle-create.component.ts
+++ src/app/components/vehicle-create/vehicle-create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { VehicleService, Vehicle } from '../../services/vehicle/vehicle.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-create',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./vehicle-create.component.scss']
 })
 export class VehicleCreateComponent {
+  vehicle: Vehicle = {
+    id: '',
+    placa: '',
+    chassi: '',
+    renavam: '',
+    modelo: '',
+    marca: '',
+    ano: new Date().getFullYear()
+  };
 
+  constructor(
+    private vehicleService: VehicleService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
+
+  createVehicle(): void {
+    this.vehicleService.createVehicle(this.vehicle).subscribe(
+      () => {
+        this.snackBar.open('Veículo criado com sucesso!', 'Fechar', {
+          duration: 3000
+        });
+        this.router.navigate(['/vehicles']);
+      },
+      (error) => {
+        this.snackBar.open('Erro ao criar o veículo. Tente novamente.', 'Fechar', {
+          duration: 3000,
+          panelClass: ['snackbar-error']
+        });
+      }
+    );
+  }
 }
